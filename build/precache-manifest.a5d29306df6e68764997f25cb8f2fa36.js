9d99053b100d76ea56436cadc5002ce2b6de87bb
Adel K. Bargach <adelk.bargach@gmail.com>
2022-08-02 16:11:10 +0200

    run build for deploy

---
+++ build/precache-manifest.a5d29306df6e68764997f25cb8f2fa36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "797d1957bec2d177cc35",
+    "url": "/fakeCord-client/static/css/main.953c332d.chunk.css"
+  },
+  {
+    "revision": "797d1957bec2d177cc35",
+    "url": "/fakeCord-client/static/js/main.797d1957.chunk.js"
+  },
+  {
+    "revision": "cd3521ad1c33c8ef6ff6",
+    "url": "/fakeCord-client/static/js/1.cd3521ad.chunk.js"
+  },
+  {
+    "revision": "d2b3e2f8ed7d2d960872",
+    "url": "/fakeCord-client/static/js/runtime~main.d2b3e2f8.js"
+  },
+  {
+    "revision": "5880e1673690e7b02b4c27a081a3b77d",
+    "url": "/fakeCord-client/index.html"
+  }
+];
